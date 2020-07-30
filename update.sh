fd3e544a5062a9ec2a97ae185e23d9ba6cc0cbc7
Ovie <yumenooo@qq.com>
2020-07-30 20:49:19 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,6 @@
 # Date：2020年3月31日
 # Author: Ovi3
 
-# update xray and config.yml
-
-rm ./core/xray_linux_amd64 ./core/config.yaml >> /dev/null 2>&1
-
+# update xray
+rm ./core/xray_linux_amd64 >> /dev/null 2>&1
 wget https://github.com/chaitin/xray/releases/latest/download/xray_linux_amd64.zip && unzip xray_linux_amd64.zip -d ./core/ && rm xray_linux_amd64.zip
-./core/xray_linux_amd64 version
-python3 ./core/modify.py
